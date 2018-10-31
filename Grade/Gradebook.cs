7c0d30d76054dda0d172941b4b803718e661530c
kyterentiev <terentjev.kiril@gmai.com>
2018-10-31 22:24:24 +0100

    grade

---
+++ Grade/Gradebook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+   public class Gradebook
+    {
+
+
+        public Gradebook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            stats.HighestGrade = 0;
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                
+                {
+                    stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                    stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                }
+
+               sum += grade;
+
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+
+        public GradeStatistics GradeStatistics { get; private set; }
+    }
+}
---
+++ Grades.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grades.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grades.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d0764d6-7978-4db8-b446-b78fcd0214cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
